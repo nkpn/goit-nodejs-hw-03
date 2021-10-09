a13af827a63168623bddf7eb7932aa70e7a79e6a
nkpn <illinmikita@gmail.com>
2021-10-09 20:31:42 +0300

    All project wad updated for use with Mongo Compass

---
+++ model/db.js
@@ -0,0 +1,28 @@
+// const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.URI_DB;
+
+const db = MongoClient.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+process.on('SIGINT', async () => {
+  const client = await db;
+  client.close();
+  console.log('Connection to DB closed');
+});
+
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// client.connect(err => {
+//   const collection = client.db('test').collection('devices');
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+module.exports = db;
